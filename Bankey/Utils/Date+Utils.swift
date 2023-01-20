a38a85f252e5fd7fc1ff9e2f92d12851ce7c6c88
tullahnazari <tnazari@prometheum.com>
2023-01-20 06:56:47 -0600

    feat: add networking to account header and summary

---
+++ Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by tullah nazari on 1/19/23.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
